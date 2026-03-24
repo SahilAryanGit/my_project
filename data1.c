f6928a47d9710618d56a2c95f695e2d32ab3df1b
SahilAryanBiker <sahilaryanbiker@gmail.com>
2026-03-24 04:08:30 -0400

    JIRA-1025 Sahil data1.c prime_number

--- data1.c
+++ data1.c
@@ -1,3 +1,23 @@
-asklasklaLSLASLALSLSLKS
-S;LKAS;LASALS
-LK;LASLKASKLAS
+#include <stdio.h>
+int main()
+{
+  int n, i;
+
+  print("Enter the num: \n");
+  scanf("%d", &n);
+
+  if(n < 2)
+    print("%d is a not Prime Number!\n", n);
+
+  for(i=2; i*i <= n; i++)
+    if(n%i == 0)
+      break; 
+
+  if(i*i > n)
+    print("%d is a Prime Number!\n", n);
+  else
+    print("%d is a not Prime Number!\n", n);
+
+
+  return 0;
+}
